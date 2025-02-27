a98e5d2b7f7448f92c5a564b23f7af3a5ec830e1
sampozz <samuele.pozzani@gmail.com>
2025-02-27 16:05:44 +0100

    Moved auth logic from userProvider to userService

--- lib/global_providers/user/user_service.dart
+++ lib/global_providers/user/user_service.dart
@@ -3,6 +3,27 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dima_project/global_providers/user/user_model.dart';
 
 class UserService {
+  /// This method will sign in the user with the provided email and password
+  Future<auth.UserCredential?> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    auth.UserCredential? userCredential;
+    // Sign in with email and password
+    try {
+      userCredential = await auth.FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      // TODO: Handle authentication error
+    }
+    return userCredential;
+  }
+
+  /// This method will sign out the user
+  Future<void> signOut() async {
+    await auth.FirebaseAuth.instance.signOut();
+  }
+
   /// This method will fetch the user data from Firestore
   /// If the user is not found in Firestore, the method will return null
   /// If the user is found in Firestore, the method will return a User model object
