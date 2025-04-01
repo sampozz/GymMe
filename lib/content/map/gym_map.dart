b51ec4111c8fe3b757fc809af884e27db734a946
dadebarzan <dade.ipodgo@gmail.com>
2025-04-01 17:06:05 +0200

    Fake mantenimento della schermata mappa attiva

--- lib/content/map/gym_map.dart
+++ lib/content/map/gym_map.dart
@@ -14,27 +14,55 @@ class GymMap extends StatefulWidget {
 
 class _GymAppState extends State<GymMap> {
   GoogleMapController? mapController;
-  final Map<String, Marker> _markers = {};
+  MapProvider? mapProvider;
+  Map<String, Marker> _markers = {};
   List<Gym>? gymList;
   LatLng _currentPosition = const LatLng(45.46427, 9.18951);
+  double _currentZoom = 14;
   bool _locationGranted = false;
+  bool initialized = false;
 
   @override
   void initState() {
     super.initState();
+
+    mapProvider = context.read<MapProvider>();
+    initialized = mapProvider?.isInitialized ?? false;
+
+    if (!initialized) {
+      print("enter here");
+      _loadGymList();
+    } else {
+      _currentPosition = mapProvider?.savedPosition ?? _currentPosition;
+      _currentZoom = mapProvider?.savedZoom ?? _currentZoom;
+    }
+
     _getUserLocation();
-    _loadGymList();
+  }
+
+  @override
+  void dispose() {
+    mapProvider?.saveMapState(_currentPosition, _currentZoom);
+    
+    super.dispose();
   }
 
   Future<void> _getUserLocation() async {
     try {
-      final position = await context.read<MapProvider>().getUserLocation();
+      final position = await mapProvider?.getUserLocation();
 
       if (position != null) {
-        setState(() {
-          _currentPosition = LatLng(position.latitude, position.longitude);
-          _locationGranted = true;
-        });
+        print(initialized);
+        if (!initialized) {
+          setState(() {
+            _currentPosition = LatLng(position.latitude, position.longitude);
+            _locationGranted = true;
+          });
+        } else {
+          setState(() {
+            _locationGranted = true;
+          });
+        }
       } else {
         setState(() {
           _locationGranted = false;
@@ -67,23 +95,14 @@ class _GymAppState extends State<GymMap> {
     }
     
     try {
-      final gyms = await context.read<MapProvider>().getGymLocations(gymList);
-      
-      if (mounted) {
-        setState(() {
-          _markers.clear();
-          for (final gym in gyms.gyms) {
-            final marker = Marker(
-              markerId: MarkerId(gym.id),
-              position: LatLng(gym.lat, gym.lng),
-              infoWindow: InfoWindow(
-                title: gym.name,
-                snippet: gym.address,
-              ),
-            );
-            _markers[gym.id] = marker;
-          }
-        });
+      if (mapProvider != null) {
+        final gyms = await mapProvider!.getGymLocations(gymList);
+        
+        if (mounted) {
+          setState(() {
+            _markers = mapProvider!.getMarkers(gyms);
+          });
+        }
       }
     } catch (e) {
       print('Error loading gym locations: $e');
@@ -94,13 +113,20 @@ class _GymAppState extends State<GymMap> {
       }
     }
   }
+
+  void _onCameraMove(CameraPosition position) {
+      setState(() {
+        _currentPosition = position.target;
+        _currentZoom = position.zoom;
+      });
+  }
   
   void _updateCameraPosition() {
     if (mapController != null) {
       mapController!.animateCamera(CameraUpdate.newCameraPosition(
         CameraPosition(
           target: _currentPosition,
-          zoom: 14,
+          zoom: _currentZoom,
         ),
       ));
     }
@@ -108,17 +134,62 @@ class _GymAppState extends State<GymMap> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement map interface
-    return GoogleMap(
-      onMapCreated: _onMapCreated,
-      cloudMapId: '7a4015798822680c',
-      initialCameraPosition: CameraPosition(
-        target: _currentPosition,
-        zoom: 14,
-      ),
-      markers: _markers.values.toSet(),
-      myLocationEnabled: _locationGranted,
-      myLocationButtonEnabled: true,
+    return Stack(
+      children: [
+        // La mappa come primo elemento (in background)
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          onCameraMove: _onCameraMove,
+          cloudMapId: '7a4015798822680c',
+          initialCameraPosition: CameraPosition(
+            target: _currentPosition,
+            zoom: _currentZoom,
+          ),
+          markers: _markers.values.toSet(),
+          myLocationEnabled: _locationGranted,
+          zoomControlsEnabled: false,
+          minMaxZoomPreference: const MinMaxZoomPreference(11, 20),
+        ),
+        
+        Positioned(
+          right: 16,
+          bottom: 100,
+          child: FloatingActionButton(
+            onPressed: _locationGranted ? () async {
+              final position = await mapProvider?.getUserLocation();
+              if (position != null) {
+                setState(() {
+                  _currentPosition = LatLng(position.latitude, position.longitude);
+                });
+              }
+              _updateCameraPosition();
+            } : null,
+            backgroundColor: _locationGranted ? Colors.blue : Colors.grey,
+            foregroundColor: _locationGranted ? Colors.white : Colors.grey.shade300,
+            tooltip: _locationGranted 
+                ? 'Go to user location'
+                : 'Access to user location denied',
+            child: const Icon(Icons.my_location),
+          ),
+        ),
+        
+        Positioned(
+          top: 48,
+          left: 16,
+          right: 16,
+          child: Card(
+            elevation: 4,
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Cerca palestre...',
+                prefixIcon: Icon(Icons.search),
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/global_providers/map_provider.dart
+++ lib/global_providers/map_provider.dart
@@ -1,15 +1,25 @@
 import 'package:dima_project/content/map/map_service.dart';
 import 'package:dima_project/content/home/gym/gym_model.dart';
 import 'package:dima_project/content/map/location_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 
 class MapProvider with ChangeNotifier {
   final MapService _mapService;
   Locations? _gymLocationsList;
+  Position? _userLocation;
+  LatLng? _savedPosition;
+  double? _savedZoom;
+  final Map<String, Marker> _markers = {};
+  bool _isInitialized = false;
 
   MapProvider({MapService? mapService})
     : _mapService = mapService ?? MapService();
+
+  LatLng get savedPosition => _savedPosition ?? const LatLng(45.46427, 9.18951);
+  double get savedZoom => _savedZoom ?? 14.0;
+  bool get isInitialized => _isInitialized;
 
   Future<Locations> getGymLocations(List<Gym>? gymList) async {
     var data = await _mapService.fetchGymLocations(gymList);
@@ -20,7 +30,38 @@ class MapProvider with ChangeNotifier {
 
   Future<Position?> getUserLocation() async {
     var data = await _mapService.fetchUserLocation();
+    _userLocation = data;
     notifyListeners();
     return data;
+  }
+
+  Map<String, Marker> getMarkers(Locations gyms) {
+    if (!_isInitialized) {
+      _isInitialized = true;
+      _markers.clear();
+      for (final gym in gyms.gyms) {
+        final marker = Marker(
+          markerId: MarkerId(gym.id),
+          position: LatLng(gym.lat, gym.lng),
+          infoWindow: InfoWindow(
+            title: gym.name,
+            snippet: gym.address,
+          ),
+        );
+        _markers[gym.id] = marker;
+      }
+      notifyListeners();
+    }
+    return _markers;
+  }
+
+  void saveMapState(LatLng? position, double? zoom) {
+    _savedPosition = position;
+    _savedZoom = zoom;
+  }
+
+  void setInitialized(bool value) {
+    _isInitialized = value;
+    notifyListeners();
   }
 }
